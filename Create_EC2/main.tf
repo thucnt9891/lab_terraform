73ea07913113f9191995f29f97ba077d50e51498
thucnt9891 <thucnt9891@gmail.com>
2022-11-24 16:44:54 +0700

    update tf create ec2

--- Create_EC2/main.tf
+++ Create_EC2/main.tf
@@ -1,1 +1,13 @@
-test ok 23/11/2022
+provider "aws" {
+  region = "ap-southeast-1"
+  access_key = "AKIA4BOZ6OZZJOYB2UIZ"
+  secret_key = "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"
+}
+
+resource "aws_instance" "hello" {
+  ami           = "ami-07651f0c4c315a529"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "Topica2022"
+  }
+}
