4a814fecfd3236fd31eebe5a41b790af3d9b2764
thucnt9891 <thucnt9891@gmail.com>
2022-11-24 17:12:52 +0700

    update main.tf 01

--- Create_EC2/main.tf
+++ Create_EC2/main.tf
@@ -1,10 +1,10 @@
 provider "aws" {
   region = "ap-southeast-1"
-  access_key = "AKIA4BOZ6OZZJOYB2UIZ"
-  secret_key = "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"
+  access_key = ""
+  secret_key = ""
 }
 
-resource "aws_instance" "hello" {
+resource "aws_instance" "Create_ec2_TF" {
   ami           = "ami-07651f0c4c315a529"
   instance_type = "t2.micro"
   tags = {
